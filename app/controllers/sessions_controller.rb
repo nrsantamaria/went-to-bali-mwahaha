a5aa288e2e08e1a068ec2d6615a7c3415cc1938c
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-14 09:25:33 -0700

    add flash notices for sign in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      flash[:notice] = "Welcome back!"
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:alert] = "There was a problem signing in. Try again."
       redirect_to '/sign_in'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "You are signed out. See you later!"
     redirect_to '/sign_in'
   end
 
