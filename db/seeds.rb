2dec979a6c825593b71f5863f2e549ef914749db
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-14 14:24:52 -0700

    add admin account to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,17 @@ class Seed
   def self.begin
     seed = Seed.new
     seed.generate_products
+    seed.add_admin
   end
 
   def generate_products
     20.times do |i|
       Product.create!(name: Faker::Beer.name, description: (Faker::Beer.style + " | " + Faker::Beer.alcohol), price: rand(5...20), image: File.new('app/assets/images/beer-icon.png'))
     end
+  end
+
+  def add_admin
+    admin = User.create({name: 'admin', email: 'admin@email.com', admin: true, password: '123456', password_confirmation: '123456'})
   end
 end
 
