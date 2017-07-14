3162edbc180177ad11ba7681b2174d9a8c443517
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-14 09:54:07 -0700

    add specs for product model. pass specs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ class Seed
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Beer.name, description: Faker::Beer.style, price: rand(10...100), image: File.new('app/assets/images/beer-icon.png'))
     end
   end
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Product do
+  it { should have_many :order_items }
+  it { should have_attached_file(:image) }
+  it { should validate_attachment_content_type(:image).
+              allowing('image/png', 'image/gif').
+              rejecting('text/plain', 'text/xml') }
+end
