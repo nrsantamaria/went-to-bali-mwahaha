b143f689941bc6c40239a04849adc27feb701bd3
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-14 09:32:00 -0700

    add validation to user model for unique emails. pass specs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
+  validates :email, :presence => true, :uniqueness => true
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
