479573bdf0ea7d27392a8f0720ac94b1a9b91c1c
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-14 16:03:57 -0700

    create factories for testing feature specs.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,30 @@
 FactoryGirl.define do
-  
+  factory :user do
+    name('Admin')
+    email('admin1@email.com')
+    password_digest('123456')
+    admin true
+    id 5
+  end
+
+  factory :product do
+    name('Beer')
+    description('Delicious')
+    price('5.50')
+    id 5
+    avatar File.new(Rails.root + 'spec/support/missing.png')
+  end
+
+  factory :order do
+    total_price('5.50')
+    user_id 5
+    id 5
+  end
+
+
+  factory :order_items do
+    quantity 1
+    product_id 5
+    order_id 5
+  end
 end
